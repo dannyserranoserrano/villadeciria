8728089420c591539ebd5fcef564dca879b622be
Daniel Serrano Serrano <98311363+dannyserranoserrano@users.noreply.github.com>
2024-09-06 23:56:08 +0200

    Carga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Componente de la barra de navegación
+import Footer from './components/Footer'; // Componente del pie de página
 import Home from './pages/Home';   // Página de inicio
 import Galeria from './pages/Galeria'; // Página de la galería de imágenes
 import About from './pages/About';  // Página "Acerca de"
@@ -10,6 +12,8 @@ function App() {
   return (
     <Router>
       <div className="app-container">
+        {/* Navbar */}
+        <Navbar />
         {/* Definición de rutas para cada página */}
         <Routes>
           <Route path="/" element={<Home />} /> {/* Ruta para el componente Home */}
@@ -17,6 +21,8 @@ function App() {
           <Route path="/about" element={<About />} /> {/* Ruta para la página Acerca de */}
           <Route path="/contact" element={<Contact />} /> {/* Ruta para la página de contacto */}
         </Routes>
+        {/* Footer */}
+        <Footer />
       </div>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import '../styles/Index.css'; // Estilos específicos para la página Home
-import Navbar from '../components/Navbar'; // Componente de la barra de navegación
-import Footer from '../components/Footer'; // Componente del pie de página
+
 import ImageGallery from '../components/ImageGallery'; // Componente para la galería de imágenes
 import VideoGallery from '../components/VideoGallery'; // Componente para la galería de videos
 
 const Index = () => {
   return (
     <div className="home-container">
-      {/* Navbar */}
-      <Navbar />
 
       {/* Sección de bienvenida */}
       <header className="home-header">
-        <h1>Bienvenidos a [Nombre del Pueblo]</h1>
+        <h1>Bienvenidos a la Villa de Ciria</h1>
         <p>Explora la belleza de nuestro pueblo a través de imágenes y videos.</p>
       </header>
 
@@ -33,13 +30,11 @@ const Index = () => {
       <section className="info-section">
         <h2>Acerca de Nuestro Pueblo</h2>
         <p>
-          Nuestro pueblo es un lugar lleno de historia, cultura y naturaleza. 
+          Nuestro pueblo es un lugar lleno de historia, cultura y naturaleza.
           Con paisajes hermosos y una comunidad acogedora, te invitamos a conocer más sobre nosotros.
         </p>
       </section>
 
-      {/* Footer */}
-      <Footer />
     </div>
   );
 };
